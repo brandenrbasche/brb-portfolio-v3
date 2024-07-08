2d974d5672ce68826eaf5bd5e9d0385526e051f7
Branden Basche <brandenbasche@gmail.com>
2024-07-08 18:00:05 -0400

    removed twMerge from horizontaltext component

--- brb-portfolio-3/components/ui/HorizontalText.tsx
+++ brb-portfolio-3/components/ui/HorizontalText.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import {twMerge} from "tailwind-merge";
 
 type Props = {
     words: string[];
-    animationSpeed: string;
 }
 
-const HorizontalText = ({ words, animationSpeed }: Props) => {
+const HorizontalText = ({ words }: Props) => {
     return (
         <div
             className='h-full w-full flex infline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
-            <ul className={twMerge('flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll', animationSpeed)}>
+            <ul className='flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll'>
                 {words.map((word, i) => (
                     <li key={i}>
                         <p className='font-black uppercase text-nowrap text-8xl'>{word}</p>
-                        {/*<p className='uppercase text-nowrap lg:text-8xl md:text-5xl sm:text-4xl'>{word}</p>*/}
                     </li>
                 ))}
             </ul>
-            <ul className={twMerge('flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll', animationSpeed)}>
+            <ul className='flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll'>
                 {words.map((word, i) => (
                     <li key={i}>
                         <p className='font-black uppercase text-nowrap text-8xl'>{word}</p>
-                        {/*<p className='uppercase text-nowrap lg:text-6xl md:text-5xl sm:text-4xl'>{word}</p>*/}
                     </li>
                 ))}
             </ul>
