1c1feeab0ff1d18acc280796300a7d11217e0bd5
Branden Basche <brandenbasche@gmail.com>
2024-07-12 10:33:09 -0400

    added hover interactions to contact cards

--- brb-portfolio-3/components/Contact.tsx
+++ brb-portfolio-3/components/Contact.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import BigButton from "@/components/ui/BigButton";
-import {ScreenFitText} from "@/components/ui/ScreenFitText";
-import Link from "next/link";
 import ContactCard from "@/components/ui/ContactCard";
 import Footer from "@/components/Footer";
 
@@ -19,9 +16,21 @@ const Contact = () => {
                     <p className='text-sm font-light text-right
                     '>Have any questions? Reach out!<br /><span className='italic text-white/50'>(I reply fast)</span></p>
                     <div className='flex space-x-3 justify-end'>
-                        <ContactCard website='LinkedIn' link='https://www.linkedin.com/in/brandenbasche/' icon={<LinkedInIcon />} />
-                        <ContactCard website='GitHub' link='https://github.com/brandenrbasche' icon={<GitHubIcon />} />
-                        <ContactCard website='Email' link='mailto:brandenbasche@gmail.com' icon={<EmailIcon />} />
+                        <ContactCard
+                            website='LinkedIn'
+                            link='https://www.linkedin.com/in/brandenbasche/'
+                            icon={<LinkedInIcon />}
+                        />
+                        <ContactCard
+                            website='GitHub'
+                            link='https://github.com/brandenrbasche'
+                            icon={<GitHubIcon />}
+                        />
+                        <ContactCard
+                            website='Email'
+                            link='mailto:brandenbasche@gmail.com'
+                            icon={<EmailIcon />}
+                        />
                     </div>
                 </div>
             </div>
@@ -32,7 +41,7 @@ const Contact = () => {
 
 const LinkedInIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 464 480">
+        <svg className='group-hover:text-[#0072b1] transition ease-in-out' xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 464 480">
             <path fill="currentColor"
                   d="M462 274v170h-98V285q0-67-50-67q-37 0-51 36q-3 7-3 24v166h-99q1-269 0-297h99v42l-1 1h1v-1q32-49 89-49q51 0 82 34t31 100zM58 4Q33 4 17.5 18.5T2 55t15 37t39 15h1q25 0 40.5-15T113 55q-1-22-16-36.5T58 4zM7 444h99V147H7v297z"/>
         </svg>
@@ -41,8 +50,8 @@ const LinkedInIcon = () => {
 
 const GitHubIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 56 56">
-            <path fill="currentColor" fill-rule="evenodd"
+        <svg className='group-hover:text-[#e37222] transition ease-in-out' xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 56 56">
+            <path fill="currentColor"
                   d="M3 28c0 11.046 7.163 20.416 17.097 23.722c1.25.23 1.707-.542 1.707-1.205c0-.593-.022-2.166-.034-4.251c-6.954 1.51-8.421-3.352-8.421-3.352c-1.138-2.889-2.777-3.658-2.777-3.658c-2.27-1.55.172-1.519.172-1.519c2.51.177 3.83 2.577 3.83 2.577c2.23 3.82 5.851 2.716 7.276 2.076c.227-1.614.873-2.716 1.586-3.34c-5.55-.631-11.387-2.777-11.387-12.357c0-2.729.974-4.962 2.574-6.708c-.258-.632-1.116-3.175.245-6.616c0 0 2.098-.673 6.874 2.563c1.994-.556 4.133-.832 6.259-.843c2.124.01 4.262.287 6.258.843c4.773-3.236 6.868-2.563 6.868-2.563c1.365 3.44.507 5.984.25 6.616c1.603 1.746 2.57 3.98 2.57 6.708c0 9.605-5.846 11.718-11.414 12.336c.896.772 1.696 2.298 1.696 4.63c0 3.342-.031 6.038-.031 6.858c0 .669.451 1.447 1.719 1.203C45.844 48.407 53 39.043 53 28C53 14.194 41.806 3 27.998 3C14.194 3 3 14.193 3 28"/>
         </svg>
     )
@@ -50,8 +59,8 @@ const GitHubIcon = () => {
 
 const EmailIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16">
-            <path fill="currentColor" fill-rule="evenodd"
+        <svg className='group-hover:text-[#c71610] transition ease-in-out' xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16">
+            <path fill="currentColor"
                   d="M14.95 3.684L8.637 8.912a1 1 0 0 1-1.276 0l-6.31-5.228A.999.999 0 0 0 1 4v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a.999.999 0 0 0-.05-.316M2 2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2m-.21 1l5.576 4.603a1 1 0 0 0 1.27.003L14.268 3z"/>
         </svg>
     )
--- brb-portfolio-3/components/ui/ContactCard.tsx
+++ brb-portfolio-3/components/ui/ContactCard.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const ContactCard = ({ website, link, icon }: Props) => {
     return (
-        <Link href={link}>
-            <div className='p-4 rounded-xl bg-white/10 text-white text-sm font-light h-52 w-40 flex flex-col justify-between'>
-                <div>{icon}</div>
-                <p>{website}</p>
+        <Link href={link} className='group' target="_blank">
+            <div className='p-4 rounded-xl bg-white/10 group-hover:bg-white/15 transition ease-in-out text-white text-sm font-light h-52 w-40 flex flex-col justify-between'>
+                <div >{icon}</div>
+                <p className='font-extralight'>{website}</p>
             </div>
         </Link>
     );
